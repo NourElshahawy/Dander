9115a56033bf4eb59978c0a6fa69a5baff5dd4ab
NourElshahawy <enour1847@gmail.com>
2024-09-22 14:44:25 +0300

    the new edit

--- assets/js/allProducts.js
+++ assets/js/allProducts.js
@@ -1,5 +1,5 @@
 let featuredProducts = [];
-fetch("https://nourelshahawy.github.io/Dander/assets/E_commerce.JSON")
+fetch("./assets/E_Commerce.JSON")
   .then((res) => res.json())
   .then((res) => {
     const products = res.featuredProducts;
